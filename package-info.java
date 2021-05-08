d69acc3c46ebb79835cbf15e380a8e9b86ee2957
Patrick Gemmll <patrick.gemmell@mths.ca>
2021-05-08 00:31:43 +0000

    v1.0

---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+* This program is a guess the number game with a random number generator
+* with customizable dice choice.
+*
+* @author  Patrick Gemmell
+* @version 1.0
+* @since   2021-05-07
+*/
+package ca.mths.unit1.unit302.java.factorial;
